5e7f3a988433a72f2b6b1f638d4089075164f0ad
thuyenvuanh <thuyenvuanh2412@gmail.com>
2024-07-18 23:46:54 +0700

    update script

--- buildAndStartApp.sh
+++ buildAndStartApp.sh
@@ -1,5 +1,5 @@
 echo Building dental-booking-app image;
-docker build -t "dental-booking app" .;
+docker build -t "dental-booking-app" .;
 
 echo Starting container;
 docker run -d -p 5000:5000 --name 'dba' dental-booking-app;
